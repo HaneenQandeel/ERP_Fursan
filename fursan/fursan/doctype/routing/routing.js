c4103d2aee0cdd4a3f561ed5ebdd63107d941204
HaneenQandeel <haneenqandeel2@gmail.com>
2018-03-27 15:58:13 +0300

    routing edit

--- fursan/fursan/doctype/routing/routing.js
+++ fursan/fursan/doctype/routing/routing.js
@@ -4,5 +4,10 @@
 frappe.ui.form.on('Routing', {
 	refresh: function(frm) {
 
+	},
+	routing_name :function(frm) { 
+		cur_frm.doc.routing_name = cur_frm.doc.from + cur_frm.doc.to
+		frm.setvalue("routing_name",cur_frm.doc.routing_name)
 	}
+
 });
